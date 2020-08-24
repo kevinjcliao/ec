c0d2ea943dcbf33339905c7a6ad09021ac6ce26e
Tim Crawford <tcrawford@system76.com>
2020-08-24 06:53:36 -0600

    battery: Implement charging thresholds
    Battery Charging Start Threshold:
    - Valid values are [0, 100]
    - A value of 0 disables the feature, leaving the charger enabled on AC
    
    Battery Charging End Threshold:
    - Valid values are [0,100]
    - A value of 100 disabled the feature, leaving the charger enabled on AC
    
    The stop threshold must be configured for the start threshold to work.

--- src/board/system76/common/power.c
+++ src/board/system76/common/power.c
@@ -350,7 +350,7 @@ void power_event(void) {
             battery_charger_disable();
         } else {
             DEBUG("plugged in\n");
-            battery_charger_enable();
+            battery_charger_configure();
         }
         battery_debug();
 
@@ -369,6 +369,11 @@ void power_event(void) {
     ac_last = ac_new;
 
     gpio_set(&AC_PRESENT, !ac_new);
+
+    // Configure charger based on charging thresholds when plugged in
+    if (!ac_new) {
+        battery_charger_configure();
+    }
 
     // Read power switch state
     static bool ps_last = true;
